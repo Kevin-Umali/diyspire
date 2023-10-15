09613121e4f8f244418903b9ce9599a477807a9b
Kevin-Umali <kevinumali0999@gmail.com>
2023-10-15 17:16:06 +0800

    Fix generated idea api issue

--- server/src/schema/community.schema.ts
+++ server/src/schema/community.schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 import { createNonRequiredString } from "../utils";
 
 export const CommunityGeneratedIdeaSchema = z.object({
-  limit: createNonRequiredString().optional().default("1"),
-  orderBy: z.enum(["asc", "desc"]).optional().default("asc"),
+  limit: createNonRequiredString().optional(),
+  orderBy: z.enum(["asc", "desc"]).optional(),
 });
 
 export type CommunityGeneratedIdeaRequest = z.infer<typeof CommunityGeneratedIdeaSchema>;
