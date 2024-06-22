114d013a2673ab08c080cb69f03eaf7aeb61377f
Kevin-Umali <kevinumali0999@gmail.com>
2024-06-23 00:37:25 +0800

    hotfix compile error

--- client/app/maintenance/page.tsx
+++ client/app/maintenance/page.tsx
@@ -1,10 +1,6 @@
-"use client";
-
 import type { Metadata } from "next";
-import { useRouter } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
+import Maintenance from "./maintenance";
 
 export const metadata: Metadata = {
   title: "Maintenance",
@@ -23,19 +19,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  const { refresh } = useRouter();
-
-  return (
-    <div className="flex min-h-screen items-center justify-center py-5 sm:py-10">
-      <div className="p-6 text-center">
-        <Label className="mb-2 block text-2xl font-bold">Under Maintenance</Label>
-        <Label className="text-md my-4 block">You can try to refresh the page to see if the issue is resolved.</Label>
-        <div className="mt-4 flex flex-col space-y-2">
-          <Button onClick={refresh} className="w-full border px-4 py-2">
-            Refresh
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+  return <Maintenance />;
 }
